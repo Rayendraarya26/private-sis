07bb1e807ea471987e6778eacd41678894107792
Aldino Kemal <aldinokemal2104@gmail.com>
2021-10-01 09:39:55 +0700

    [develop] Login: add validate email domain

--- Modules/Auth/Http/Controllers/RegisterController.php
+++ Modules/Auth/Http/Controllers/RegisterController.php
@@ -26,7 +26,7 @@ class RegisterController extends Controller
     {
         $request->validate([
             "fullname" => 'required|string',
-            "email"    => 'required|email|unique:App\Models\BbkkpSis\SysUser,user_email',
+            "email"    => 'required|email:dns|unique:App\Models\BbkkpSis\SysUser,user_email',
             "password" => 'required|confirmed|min:4'
         ]);
 
@@ -39,39 +39,39 @@ class RegisterController extends Controller
         // 1
         try {
             DB::beginTransaction();
-            $user = new SysUser();
-            $user->user_email = strtolower($request['email']);
-            $user->user_fullname = ucwords($request['fullname']);
-            $user->user_password = bcrypt($request['password']);
-            $user->user_token = Str::random(20);
-            $user->user_is_active = "no";
-            $user->user_is_banned = "no";
+            $user                  = new SysUser();
+            $user->user_email      = strtolower($request['email']);
+            $user->user_fullname   = ucwords($request['fullname']);
+            $user->user_password   = bcrypt($request['password']);
+            $user->user_token      = Str::random(20);
+            $user->user_is_active  = "no";
+            $user->user_is_banned  = "no";
             $user->user_created_at = Carbon::now();
             $user->save();
 
-            $ug = new SysUserGroup();
-            $ug->ug_user_id = $user->user_id;
-            $ug->ug_group_id = 2;
+            $ug                = new SysUserGroup();
+            $ug->ug_user_id    = $user->user_id;
+            $ug->ug_group_id   = 2;
             $ug->ug_is_default = "yes";
             $ug->ug_created_at = Carbon::now();
             $ug->save();
 
-            $sisPelanggan = new SisPelanggan();
-            $sisPelanggan->user_id = $user->user_id;
+            $sisPelanggan             = new SisPelanggan();
+            $sisPelanggan->user_id    = $user->user_id;
             $sisPelanggan->cust_email = $user->user_email;
             $sisPelanggan->save();
             DB::commit();
 
 
             // ================ Send Email ================
-            $structEmail = new EmailStruct();
+            $structEmail          = new EmailStruct();
             $structEmail->subject = "Pendaftaran berhasil";
-            $structEmail->body = view('auth::mails.register_success')
+            $structEmail->body    = view('auth::mails.register_success')
                 ->with([
                     'name' => $user->user_fullname,
                     'link' => route('auth.verify', encrypt($user->user_token))
                 ])->render();
-            $structEmail->to = $user->user_email;
+            $structEmail->to      = $user->user_email;
             sendEmail($structEmail);
             // ================ END Send Email ================
 
