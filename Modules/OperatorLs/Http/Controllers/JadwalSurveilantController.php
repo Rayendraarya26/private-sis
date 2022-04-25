30644d5d6e20d919b7bc0c325d0f68cceb3990ec
Aldino Kemal <aldinokemal2104@gmail.com>
2022-04-25 22:09:08 +0700

    feat: add page jadwal surveilant
    commit 68e9c3eef12d7257849bf68955e025b99053d4d1
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 22:08:03 2022 +0700
    
        feat: add company name in email title
    
    commit 8233d38c7fb04c65169e0811fd31652423c86908
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 22:06:12 2022 +0700
    
        feat: add email send notif to finance
    
    commit f5226fb4b6a3651526a370db9905f84d6797edd9
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 21:03:05 2022 +0700
    
        feat: add field reminder internal surveilant
    
    commit b2f277a110b10353e2bb43779651a798b632788e
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 20:45:54 2022 +0700
    
        feat: add filter is billing terbuat
    
    commit 4540ed1b1dc4b9801cc744bf0165a1f410bf8f2e
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 18:54:58 2022 +0700
    
        fix: wrong syntax
    
    commit 7c35df47ccac4412df3772fb8e8dd9805d691d26
    Author: Aldino Kemal <aldinokemal2104@gmail.com>
    Date:   Mon Apr 25 18:46:31 2022 +0700
    
        feat: add page jadwal surveilant

---
+++ Modules/OperatorLs/Http/Controllers/JadwalSurveilantController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Modules\OperatorLs\Http\Controllers;
+
+use App\Http\Structs\BreadcrumbsStruct;
+use App\Http\Structs\EmailStruct;
+use App\Http\Structs\NotifStruct;
+use App\Models\BbkkpSis\SisPelangganSertifikasi;
+use App\Models\BbkkpSis\SysUserGroup;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+
+class JadwalSurveilantController extends Controller
+{
+    public $module = self::class;
+    private $url = 'operatorls/jadwal-surveilant';
+    private $view = "operatorls::jadwal_surveilant";
+
+    public function index()
+    {
+        $breadcrumbs = [
+            new BreadcrumbsStruct('Operator Lembaga Sertifikasi'),
+            new BreadcrumbsStruct('Jadwal Surveilant'),
+        ];
+
+        $parser = ['module' => $this->module, 'url' => $this->url, 'breadcrumbs' => $breadcrumbs];
+        return view("$this->view.index")->with($parser);
+    }
+
+    public function reminderFinance(Request $request)
+    {
+        $request->validate(['cust_sert_id' => 'required|int']);
+        try {
+            $data = SisPelangganSertifikasi::with(['sis_pelanggan.sys_user', 'master_sertifikasi'])->find($request['cust_sert_id']);
+            if (empty($data)) throw new \Exception("Data sertifikat tidak ditemukan");
+
+            // Send Notification to Keuangan
+            $groupUsers = SysUserGroup::with('user')->whereIn('ug_group_id', [7])->get();
+            if ($groupUsers) {
+                foreach ($groupUsers as $group) {
+                    $notifStruct = new NotifStruct();
+                    // Send Push
+                    $notifStruct->title     = "Reminder Billing Surveilant " . $data?->sis_pelanggan?->cust_nama;
+                    $notifStruct->message   = sprintf("Mohon segera terbitkan billing surveilant untuk sertifikat <b>%s</b> pada perusahaan <b>%s</b> yang akan dilaksanakan tanggal %s.", $data->master_sertifikasi->sert_nama, $data?->sis_pelanggan?->cust_nama, $data->cust_sert_survailen_date->isoFormat("LL"));
+                    $notifStruct->user_id   = $group?->ug_user_id;
+                    $notifStruct->click_url = url('/keuangan/billing/create');
+
+                    sendNotification($notifStruct);
+
+                    // ====================================================================================================
+
+                    // Send Email
+                    $structEmail          = new EmailStruct();
+                    $structEmail->subject = "Reminder Billing Surveilant " . $data?->sis_pelanggan?->cust_nama;
+                    $structEmail->body    = view("$this->view.mails.finance_create_billing")
+                        ->with([
+                            'cert_name'      => $data->master_sertifikasi->sert_nama,
+                            'company'        => $data?->sis_pelanggan?->cust_nama,
+                            'tgl_surveilant' => $data->cust_sert_survailen_date->isoFormat("LL"),
+                            'link'           => url('/keuangan/billing/create'),
+                        ])->render();
+                    $structEmail->to      = $group->user->user_email;
+
+                    sendEmail($structEmail);
+                }
+            }
+
+            $data->cust_sert_survailen_reminder_internal_count += 1;
+            $data->cust_sert_survailen_reminder_internal_at    = Carbon::now();
+            $data->save();
+            return responseJSON(200, [], 'Notifikasi ke Finance berhasil dikirim');
+        } catch (\Exception $e) {
+            return responseJSON(500, [], $e->getMessage());
+        }
+    }
+
+    public function ajax(Request $request)
+    {
+        $request->validate(['action' => 'required']);
+        return match ($request['action']) {
+            'datagrid' => $this->ajax_datagrid($request),
+            default    => null,
+        };
+    }
+
+    private function ajax_datagrid(Request $request)
+    {
+        $data = (new SisPelangganSertifikasi)
+            ->join('sis_pelanggan', 'sis_pelanggan.cust_id', 'sis_pelanggan_sertifikasi.cust_id')
+            ->join('master_sertifikasi', 'master_sertifikasi.sert_id', 'sis_pelanggan_sertifikasi.sert_id')
+            ->leftJoin('sis_billing_items', 'sis_billing_items.cust_sert_id', '=', 'sis_pelanggan_sertifikasi.cust_sert_id')
+            ->where("cust_sert_survailen_date", '>=', date("Y-m-d H:i:s"))
+            ->where('cust_sert_status_survailen', '=', 'passed');
+
+        // Filter
+        if (!empty($request->filterRules)) {
+            foreach (json_decode($request->filterRules) as $f) {
+                if ($f->field == "is_bill_created") {
+                    if ($f->value == "yes") {
+                        $data->whereNotNull('sis_billing_items.cust_sert_id');
+                    } else if ($f->value == "no") {
+                        $data->whereNull('sis_billing_items.cust_sert_id');
+                    }
+                } else {
+                    $data->where($f->field, 'LIKE', '%' . $f->value . '%');
+                }
+            }
+        }
+        // Sorter
+        if (!empty($request->sort) && !empty($request->order)) {
+            $sort  = explode(",", $request->sort);
+            $order = explode(",", $request->order);
+            for ($i = 0; $i < count($sort); $i++) {
+                $data->orderBy($sort[$i], $order[$i]);
+            }
+        } else {
+            $data->orderBy('cust_sert_survailen_date');
+        }
+        // Total
+        $total = $data->select(DB::raw('count(*) as total'))->first()->total;
+        // Pagination
+        $data->select(DB::raw("
+            sis_pelanggan_sertifikasi.*,
+            master_sertifikasi.sert_nama,
+            sis_billing_items.itms_bil_id,
+            sis_pelanggan.cust_nama
+            "))
+            ->skip(($request->page - 1) * $request->rows)->take($request->rows);
+
+        // Result
+        $result = [];
+        foreach ($data->get() as $d) {
+            $x['cust_sert_id']                                = $d->cust_sert_id;
+            $x['cust_id']                                     = $d->cust_id;
+            $x['cust_nama']                                   = $d->cust_nama;
+            $x['cust_sert_survailen_reminder_internal_count'] = $d->cust_sert_survailen_reminder_internal_count;
+            $x['sert_nama']                                   = $d->sert_nama;
+            $x['cust_sert_survailen_date']                    = $d->cust_sert_survailen_date->format("Y-m-d");
+            $x['is_bill_created']                             = !empty($d->itms_bil_id);
+            $result[]                                         = $x;
+        }
+
+        return response()->json(["total" => $total, "rows" => $result]);
+    }
+}
---
+++ Modules/OperatorLs/Resources/views/jadwal_surveilant/index.blade.php
@@ -0,0 +1,162 @@
+@extends('layouts.layout_app')
+
+@section('title', 'Jadwal Surveilant')
+
+@section('content')
+    <div class="dt-content">
+        <div class="row">
+            <div class="col-md-12">
+                @if(session('message'))
+                    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                        {!! session('message') !!}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                @endif
+                <div class="dt-card">
+                    <div class="dt-card__header">
+                        <div class="dt-card__heading">
+                            <h3 class="dt-card__title">Jadwal Surveilant yang akan datang</h3>
+                        </div>
+                    </div>
+                    <div class="dt-card__body">
+                        <div id="ttData" style="width:100%; min-width: 310px"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push("javascript")
+    <script>
+        function promptNotif(custSertId) {
+            const swalWithBootstrapButtons = swal.mixin({
+                confirmButtonClass: 'btn btn-success mb-2',
+                cancelButtonClass: 'btn btn-danger mr-2 mb-2',
+                buttonsStyling: false,
+            });
+
+            swalWithBootstrapButtons({
+                title: `Notifikasi Keuangan ?`,
+                text: "Kirim notifikasi (email & push notif) mengenai pembuatan billing untuk surveilant",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Kirim',
+                cancelButtonText: 'Batal',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.value) {
+                    notifApi(custSertId)
+                }
+            });
+        }
+
+        function notifApi(custSertId) {
+            let formData = new FormData();
+            formData.append('cust_sert_id', custSertId)
+            $.ajax({
+                url: `{{action("$module@reminderFinance")}}`,
+                type: 'post',
+                processData: false,
+                contentType: false,
+                data: formData,
+                success: async function (res) {
+                    toastCenter({
+                        type: 'success',
+                        title: res.message
+                    })
+
+                    $('#ttData').datagrid('reload');
+                },
+                error: function (xhr) {
+                    self.loading_submit = false;
+                    if (xhr.readyState === 0) toastCenter({type: 'error', title: "Network Error"})
+                    else toastCenter({type: 'error', 'title': xhr.responseJSON.message})
+                }
+            });
+        }
+
+        $(function () {
+            let dg = $('#ttData').datagrid({
+                method: 'get',
+                height: document.documentElement.scrollHeight - 300,
+                url: `{{ url("$url/ajax?action=datagrid") }}`,
+                rownumbers: false,
+                nowrap: false,
+                singleSelect: true,
+                remoteFilter: true,
+                multiSort: true,
+                // fitColumns: true,
+                toolbar: '#toolbar',
+                pagination: true,
+                pageSize: 50,
+                clientPaging: false,
+                frozenColumns: [[
+                        @if(authorized("$module@reminderFinance"))
+                    {
+                        field: 'action',
+                        title: "Aksi",
+                        width: 140,
+                        align: 'center',
+                        formatter: function (val, row) {
+                            if (!row.is_bill_created) {
+                                if (row.cust_sert_survailen_reminder_internal_count == 0) {
+                                    return `<button onclick="promptNotif(${row.cust_sert_id})" class="btn btn-block btn-sm"><i class="fas fa-bell"></i> Kirim</button>`
+                                } else {
+                                    return `<button onclick="promptNotif(${row.cust_sert_id})" class="btn btn-block btn-sm btn-warning"><i class="fas fa-bell"></i> (${row.cust_sert_survailen_reminder_internal_count}) Kirim Ulang</button>`
+                                }
+                            }
+                        }
+                    }
+                    @endif
+                ]],
+                columns: [[
+                    {
+                        field: 'is_bill_created', title: 'Billing', width: 100, sortable: false,
+                        formatter: function (val) {
+                            return val ? "Terbuat" : "Belum";
+                        },
+                        styler: function (val) {
+                            if (val) {
+                                return 'color:white;background-color:#2e7d32;';
+                            } else {
+                                return 'color:black;background-color:#FAAD14;';
+                            }
+                        }
+                    },
+                    {field: 'cust_nama', title: 'Nama Perusahaan', width: 320, sortable: false},
+                    {field: 'sert_nama', title: 'Sertifikasi', width: 320, sortable: false},
+                    {field: 'cust_sert_survailen_date', title: 'Tgl Surveilant', width: 150, sortable: true},
+                ]],
+            });
+            dg.datagrid(
+                'enableFilter', [
+                    {field: 'action', type: 'label'},
+                    {field: 'sert_nama', type: 'textbox'},
+                    {
+                        field: 'is_bill_created',
+                        type: 'combobox',
+                        options: {
+                            panelHeight: 'auto',
+                            data: [
+                                {value: '', text: 'Semua'},
+                                {value: 'yes', text: 'Terbuat'},
+                                {value: 'no', text: 'Belum'},
+                            ],
+                            onChange: function (value) {
+                                dg.datagrid('addFilterRule', {
+                                    field: 'is_bill_created',
+                                    op: 'equal',
+                                    value: value
+                                });
+
+                                dg.datagrid('doFilter');
+                            }
+                        }
+                    },
+                ]);
+        });
+    </script>
+@endpush
