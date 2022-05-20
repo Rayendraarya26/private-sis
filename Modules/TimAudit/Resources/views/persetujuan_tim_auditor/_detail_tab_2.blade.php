83aef8eab50ff2d67649e877fa6386ba8266a85e
Aldino Kemal <aldinokemal2104@gmail.com>
2022-05-20 13:14:41 +0700

    fix: add null safe

--- Modules/TimAudit/Resources/views/persetujuan_tim_auditor/_detail_tab_2.blade.php
+++ Modules/TimAudit/Resources/views/persetujuan_tim_auditor/_detail_tab_2.blade.php
@@ -7,43 +7,43 @@
                 <tbody>
                 <tr>
                     <td>Nama Perusahaan</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nama}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nama}}</td>
                 </tr>
                 <tr>
                     <td>Nomor Akta Pendirian</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nomor_akta_pendirian}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nomor_akta_pendirian}}</td>
                 </tr>
                 <tr>
                     <td>Nama Pemilik</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nama_pemilik}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nama_pemilik}}</td>
                 </tr>
                 <tr>
                     <td>Nama Pimpinan</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nama_pimpinan}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nama_pimpinan}}</td>
                 </tr>
                 <tr>
                     <td>Nama Wakil Manajemen</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nama_wakil_manajemen}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nama_wakil_manajemen}}</td>
                 </tr>
                 <tr>
                     <td>Telp (Perusahaan)</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nomor_telp}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nomor_telp}}</td>
                 </tr>
                 <tr>
                     <td>Fax</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nomor_fax}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nomor_fax}}</td>
                 </tr>
                 <tr>
                     <td>Nomer HP (CP)</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_nomor_hp}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_nomor_hp}}</td>
                 </tr>
                 <tr>
                     <td>Badan Hukum</td>
-                    <td>: {{$dataPemohon[0]->master_badan_hukum?->badan_hukum_nama}}</td>
+                    <td>: {{$dataPemohon[0]?->master_badan_hukum?->badan_hukum_nama}}</td>
                 </tr>
                 <tr>
                     <td>Jenis Perusahaan</td>
-                    <td>: {{$dataPemohon[0]->master_jenis_perusahaan?->jenis_perusahaan_nama}}</td>
+                    <td>: {{$dataPemohon[0]?->master_jenis_perusahaan?->jenis_perusahaan_nama}}</td>
                 </tr>
                 </tbody>
             </table>
@@ -59,36 +59,36 @@
                 <tbody>
                 <tr>
                     <td>Negara</td>
-                    <td>: {{$dataPemohon[0]->master_negara?->negara_nama}}</td>
+                    <td>: {{$dataPemohon[0]?->master_negara?->negara_nama}}</td>
                 </tr>
-                @if(strtolower($dataPemohon[0]->master_negara?->negara_nama) == "indonesia")
+                @if(strtolower($dataPemohon[0]?->master_negara?->negara_nama) == "indonesia")
                     <tr>
                         <td></td>
                         <td>Provinsi</td>
-                        <td>: {{$dataPemohon[0]->master_provinsi?->prov_nama}}</td>
+                        <td>: {{$dataPemohon[0]?->master_provinsi?->prov_nama}}</td>
                     </tr>
                     <tr>
                         <td></td>
                         <td>Kabupaten</td>
-                        <td>: {{$dataPemohon[0]->master_kabupaten?->kab_nama}}</td>
+                        <td>: {{$dataPemohon[0]?->master_kabupaten?->kab_nama}}</td>
                     </tr>
                     <tr>
                         <td></td>
                         <td>Kecamatan</td>
-                        <td>: {{$dataPemohon[0]->master_kecamatan?->kec_nama}}</td>
+                        <td>: {{$dataPemohon[0]?->master_kecamatan?->kec_nama}}</td>
                     </tr>
                 @endif
                 <tr>
                     <td>Alamat</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_alamat}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_alamat}}</td>
                 </tr>
                 <tr>
                     <td>Luas Tanah</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_luas_tanah}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_luas_tanah}}</td>
                 </tr>
                 <tr>
                     <td>Luas Bangunan</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_luas_bangunan}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_luas_bangunan}}</td>
                 </tr>
                 </tbody>
             </table>
@@ -103,28 +103,28 @@
                 <tbody>
                 <tr>
                     <td>Jumlah Shift (dalam sehari)</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_shif_kerja}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_shif_kerja}}</td>
                 </tr>
                 <tr>
                     <td>Jumlah Bagian</td>
-                    <td>: {{$dataPemohon[0]->mohon_cust_jumlah_bagian}}</td>
+                    <td>: {{$dataPemohon[0]?->mohon_cust_jumlah_bagian}}</td>
                 </tr>
                 <tr>
                     <td>
                         Jumlah Karyawan
                         <ol id="jumlah_karyawan">
-                            <li>Manajemen: {{$dataPemohon[0]->mohon_cust_jumlah_manajemen}} orang</li>
-                            <li>Administrasi: {{$dataPemohon[0]->mohon_cust_jumlah_administrasi}} orang</li>
-                            <li>Part Time: {{$dataPemohon[0]->mohon_cust_jumlah_part_time}} orang</li>
+                            <li>Manajemen: {{$dataPemohon[0]?->mohon_cust_jumlah_manajemen}} orang</li>
+                            <li>Administrasi: {{$dataPemohon[0]?->mohon_cust_jumlah_administrasi}} orang</li>
+                            <li>Part Time: {{$dataPemohon[0]?->mohon_cust_jumlah_part_time}} orang</li>
                             <li>
                                 Operasional:
                                 <ul>
-                                    <li>Shift 1: {{$dataPemohon[0]->mohon_cust_jumlah_shift_1}} orang</li>
-                                    <li>Shift 2: {{$dataPemohon[0]->mohon_cust_jumlah_shift_2}} orang</li>
-                                    <li>Shift 3: {{$dataPemohon[0]->mohon_cust_jumlah_shift_3}} orang</li>
+                                    <li>Shift 1: {{$dataPemohon[0]?->mohon_cust_jumlah_shift_1}} orang</li>
+                                    <li>Shift 2: {{$dataPemohon[0]?->mohon_cust_jumlah_shift_2}} orang</li>
+                                    <li>Shift 3: {{$dataPemohon[0]?->mohon_cust_jumlah_shift_3}} orang</li>
                                 </ul>
                             </li>
-                            <li>Non Permanen: {{$dataPemohon[0]->mohon_cust_jumlah_non_permanen}} orang</li>
+                            <li>Non Permanen: {{$dataPemohon[0]?->mohon_cust_jumlah_non_permanen}} orang</li>
                         </ol>
                     </td>
                 </tr>
@@ -134,7 +134,7 @@
     </div>
 
 
-    @if(!empty($dataPemohon[0]->sis_permohonan_pabriks))
+    @if(!empty($dataPemohon[0]?->sis_permohonan_pabriks))
         <div class="col-md-12">
             <h4>Data Pabrik</h4>
 
@@ -147,67 +147,69 @@
                         <td>
                             Detail Lokasi Pabrik :
                             <div class="table-responsive" style="padding-top: 20px">
-                                @foreach($dataPemohon[0]->sis_permohonan_pabriks as $pabrik)
-                                    <h4 style="text-align: center">Pabrik Ke
-                                        - {{$loop->iteration}}</h4>
-                                    <table class="table" style="padding-bottom: 20px">
-                                        <tbody>
-                                        <tr>
-                                            <td>Nama Pabrik</td>
-                                            <td>: {{$pabrik->mohon_pabrik_nama}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Np Telp</td>
-                                            <td>: {{$pabrik->mohon_pabrik_nomor_telp}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>No HP</td>
-                                            <td>: {{$pabrik->mohon_pabrik_nomor_hp}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Fax</td>
-                                            <td>: {{$pabrik->mohon_pabrik_nomor_fax}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Provinsi</td>
-                                            <td>: {{$pabrik->master_provinsi?->prov_nama}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Kabupaten</td>
-                                            <td>: {{$pabrik->master_kabupaten?->kab_nama}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Kecamatan</td>
-                                            <td>: {{$pabrik->master_kecamatan?->kec_nama}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>KodePos</td>
-                                            <td>: {{$pabrik->mohon_pabrik_kode_pos}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Alamat Pabrik</td>
-                                            <td>: {{$pabrik->mohon_pabrik_alamat}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jumlah Karyawan</td>
-                                            <td>: {{$pabrik->mohon_pabrik_jumlah_karyawan}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Kegiatan Utama</td>
-                                            <td>: {{$pabrik->mohon_pabrik_kegiatan_utama}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Luas Tanah</td>
-                                            <td>: {{$pabrik->mohon_pabrik_luas_tanah}}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Luas Bangunan</td>
-                                            <td>: {{$pabrik->mohon_pabrik_luas_bangunan}}</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
-                                    <br>
-                                @endforeach
+                                @if(!empty($dataPemohon[0]?->sis_permohonan_pabriks))
+                                    @foreach($dataPemohon[0]?->sis_permohonan_pabriks as $pabrik)
+                                        <h4 style="text-align: center">Pabrik Ke
+                                            - {{$loop->iteration}}</h4>
+                                        <table class="table" style="padding-bottom: 20px">
+                                            <tbody>
+                                            <tr>
+                                                <td>Nama Pabrik</td>
+                                                <td>: {{$pabrik->mohon_pabrik_nama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Np Telp</td>
+                                                <td>: {{$pabrik->mohon_pabrik_nomor_telp}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>No HP</td>
+                                                <td>: {{$pabrik->mohon_pabrik_nomor_hp}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Fax</td>
+                                                <td>: {{$pabrik->mohon_pabrik_nomor_fax}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Provinsi</td>
+                                                <td>: {{$pabrik->master_provinsi?->prov_nama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Kabupaten</td>
+                                                <td>: {{$pabrik->master_kabupaten?->kab_nama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Kecamatan</td>
+                                                <td>: {{$pabrik->master_kecamatan?->kec_nama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>KodePos</td>
+                                                <td>: {{$pabrik->mohon_pabrik_kode_pos}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Alamat Pabrik</td>
+                                                <td>: {{$pabrik->mohon_pabrik_alamat}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Jumlah Karyawan</td>
+                                                <td>: {{$pabrik->mohon_pabrik_jumlah_karyawan}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Kegiatan Utama</td>
+                                                <td>: {{$pabrik->mohon_pabrik_kegiatan_utama}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Luas Tanah</td>
+                                                <td>: {{$pabrik->mohon_pabrik_luas_tanah}}</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Luas Bangunan</td>
+                                                <td>: {{$pabrik->mohon_pabrik_luas_bangunan}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <br>
+                                    @endforeach
+                                @endif
                             </div>
                         </td>
                     </tr>
@@ -227,7 +229,7 @@
                 <tr>
                     <td>Pertanyaan Tambahan</td>
                     <td>: <a target="_blank"
-                             href="{{url(asset($dataPemohon[0]->mohon_pertanyaan_filepath))}}">
+                             href="{{url(asset($dataPemohon[0]?->mohon_pertanyaan_filepath))}}">
                             <i class="fad fa-download"></i> Unduh
                         </a>
                     </td>
