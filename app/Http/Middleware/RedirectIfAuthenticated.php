91768c1553b3b4326ef863f92e23ea7fa8542ec9
Aldino Kemal <aldinokemal2104@gmail.com>
2022-10-17 01:37:32 +0700

    feat: disable register from app if sso enabled

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -12,13 +12,16 @@ class RedirectIfAuthenticated
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  ...$guards
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param string|null ...$guards
      * @return mixed
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
+        if (config('app.sso_is_enabled')) {
+            abort(404);
+        }
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
