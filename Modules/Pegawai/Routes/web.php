34904e8efdcaef34da9ad742e4d363d07f5a88cf
Aldino Kemal <aldinokemal2104@gmail.com>
2022-10-27 09:39:22 +0700

    feat: remove unused module

--- Modules/Pegawai/Routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Pegawai\Http\Controllers\PegawaiController;
-
-Route::prefix('pegawai')->middleware(['auth', 'restrict'])->group(function () {
-    Route::get('/', [PegawaiController::class, 'index']);
-});
